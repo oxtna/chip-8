4573ed345a1ae10cdea1870a0bbb92b92dadad66
oxtna <rafalkajomof2709@gmail.com>
2023-09-21 15:13:15 +0200

    Setup SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include "chip.hpp"
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main() {
     Chip chip;
+
+    auto window = sf::RenderWindow{{640u, 320u}, "Chip-8 Emulator"};
+    window.setFramerateLimit(60);
+    while (window.isOpen()) {
+        auto event = sf::Event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 
     return 0;
 }
